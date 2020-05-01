712b3aa085408c5283e4d502ce14f9947ffceaee
NavaneethVijay <navaneeth.v@codilar.com>
2020-05-01 19:10:04 +0530

    initia commit

---
+++ app.js
@@ -0,0 +1,56 @@
+var createError = require('http-errors')
+var express = require('express')
+var exphbs = require('express-handlebars')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+const mongoose = require('mongoose')
+require('./models/UrlShorten')
+const dotenv = require('dotenv')
+dotenv.config()
+
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
+var apiRouter = require('./routes/api/index')
+
+var app = express()
+
+app.engine('handlebars', exphbs())
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'handlebars')
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+app.use('/api', apiRouter)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+
+// Setup mongoserver
+const uri = process.env.MONGODB_URI
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+module.exports = app
